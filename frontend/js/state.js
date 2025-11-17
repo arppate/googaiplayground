351496a815f21a454ea9a7032a64c54dc733afe9
arpitapatel <arpitagpatel5@gmail.com>
2025-11-18 01:26:57 +0530

    Clean up state object by removing unused properties
    Removed unused properties from the state object.

--- frontend/js/state.js
+++ frontend/js/state.js
@@ -1,8 +1,5 @@
 export const state = {
   videoId: null,
   gcsUri: null,
-  frames: [],            // array of {frame_id, image_base64, ...}
-  selectedFrame: null,   // {frame_id, image_base64}
-  selectedPlatform: null,
-  generatedImage: null,  // {image_base64, caption, tagline, image_prompt}
+  ...
 };
